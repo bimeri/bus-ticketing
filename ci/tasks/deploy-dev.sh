81cec33ec56dfeaebea5241fd9f0de49226aecce
Edward Tanko <eddytnk@gmail.com>
2019-09-04 22:13:52 -0500

    Create pipeline

---
+++ ci/tasks/deploy-dev.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+ls -l
+#echo "${LDC_TARGET}:${LDC_USERNAME}@${LDC_PASSWORD}"
+
+ldc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
+ldc push  --appDir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
+
---
+++ ci/tasks/test-and-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+ls -l version/version
+version=$(cat version/version)
+echo $version
+pwd
+ls
+cd source-code-from-gitlab/
+echo "Current directory ..."
+ls
+echo "Packaging jar file"
+./mvnw clean package
+echo "Current directory ..."
+ls
+echo "copying jar to ../jar-file directory"
+mv ./target/*.jar  ../jar-file/gowaka-$version.jar
+ls -l ../jar-file
