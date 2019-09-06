cb7f3c8cc86d623a0af22617a890c221a6fef500
Edward Tanko <eddytnk@gmail.com>
2019-09-06 04:46:15 -0500

    update deploy dev CI script

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -5,5 +5,5 @@ ls -l
 #echo "${LDC_TARGET}:${LDC_USERNAME}@${LDC_PASSWORD}"
 
 ldc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
-ldc push  --appDir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
+ldc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
 
