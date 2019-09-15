2655915a544e09bb2e9e92cec421ae7feb854473
Edward Tanko <eddytnk@gmail.com>
2019-09-14 23:50:38 -0500

    Clean up swagger UI - [#168496605]

--- src/main/java/net/gowaka/gowaka/config/SwaggerConfig.java
+++ src/main/java/net/gowaka/gowaka/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package net.gowaka.gowaka.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -45,7 +48,12 @@ public class SwaggerConfig {
                                 .modelRef(new ModelRef("string"))
                                 .parameterType("header")
                                 .required(true)
-                                .build()));
+                                .build()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .paths(Predicates.not(PathSelectors.regex("/actuator.*")))
+                .build();
     }
 
     private ApiInfo metadata() {
