a297d58b1f6b1bbdd4da1668c6d50d4ea8a5a74a
Edward Tanko <eddytnk@gmail.com>
2019-10-08 06:41:56 -0500

    Update pipeline to use latest version of LocalCloud - [#169009961]

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -5,5 +5,9 @@ ls -l
 #echo "${LDC_TARGET}:${LDC_USERNAME}@${LDC_PASSWORD}"
 
 ldc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
+ldc ldc add-env GoWakaEnv -n APP_NAME -v $APP_NAME
+ldc ldc add-env GoWakaEnv -n APP_CLIENT_ID -v $APP_CLIENT_ID
+ldc ldc add-env GoWakaEnv -n APP_CLIENT_SECRET -v $APP_CLIENT_SECRET
+ldc get-env GoWakaEnv
 ldc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
 
