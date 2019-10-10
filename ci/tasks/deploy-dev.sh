9ec6d3192fdadbfac6417f3c23bfd97901982ea0
Edward Tanko <eddytnk@gmail.com>
2019-10-10 06:41:08 -0500

    Optimize Test running time, it takes about 2mins to run all test now - [#169032981]

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -8,6 +8,8 @@ ldc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
 ldc add-env GoWakaEnv -n APP_NAME -v $APP_NAME
 ldc add-env GoWakaEnv -n APP_CLIENT_ID -v $APP_CLIENT_ID
 ldc add-env GoWakaEnv -n APP_CLIENT_SECRET -v $APP_CLIENT_SECRET
+ldc add-env GoWakaEnv -n GW_DB_USER -v $GW_DB_USER
+ldc add-env GoWakaEnv -n GW_DB_PWD -v $GW_DB_PWD
 ldc get-env GoWakaEnv
 ldc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
 
