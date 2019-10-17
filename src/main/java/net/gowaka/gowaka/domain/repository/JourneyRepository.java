29461679125f32dac6546002136da837eb69870e
Nouks <nnouka.stephen@go-groups.net>
2019-10-17 09:47:22 +0100

    **AGENCY_MANAGER or AGENCY_OPERATOR or AGENCY_BOOKING**  can view all Journey for OfficialAgency ordered by date and arrivalIndicator - [#169112516]

--- src/main/java/net/gowaka/gowaka/domain/repository/JourneyRepository.java
+++ src/main/java/net/gowaka/gowaka/domain/repository/JourneyRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
  * Author: Edward Tanko <br/>
  * Date: 9/3/19 5:11 PM <br/>
  */
+
 @Repository
 public interface JourneyRepository extends JpaRepository<Journey, Long> {
     List<Journey> findAllByOrderByTimestampDescArrivalIndicatorAsc();
