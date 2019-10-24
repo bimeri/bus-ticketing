d2e01c4ccdd2d757442c2dcf83484c458c0978d7
Edward Tanko <eddytnk@gmail.com>
2019-10-24 16:23:59 -0500

    Add Ui to GW Build pipeline

--- ci/tasks/test-and-build.sh
+++ ci/tasks/test-and-build.sh
@@ -9,6 +9,7 @@ ls
 
 export NG_CLI_ANALYTICS=ci
 cd ui-source-code-from-gitlab/
+npm config set unsafe-perm true
 npm install -g @angular/cli
 ng build --output-path gw_ui
 mv ./gw_ui/* ../source-code-from-gitlab/src/main/resources/static
