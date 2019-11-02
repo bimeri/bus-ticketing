0f88f576280257e6ece47bb965fd33639e68d7a8
Edward Tanko <eddytnk@gmail.com>
2019-11-02 02:02:45 -0500

    update build pipeline script

--- ci/tasks/test-and-build.sh
+++ ci/tasks/test-and-build.sh
@@ -10,8 +10,8 @@ ls
 export NG_CLI_ANALYTICS=ci
 cd ui-source-code-from-gitlab/
 mkdir -p gw_ui
-npm i --only=dev
 npm install -g
+npm i --only=dev
 npm install -g @angular/cli
 ng build --output-path gw_ui
 mv ./gw_ui/* ../source-code-from-gitlab/src/main/resources/static
