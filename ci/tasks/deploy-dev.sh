47aa60ad6c618db15bbb2faf8d89c4deaf37698e
Edward Tanko <eddytnk@gmail.com>
2019-11-30 18:36:48 -0600

    Update deployment script

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -4,12 +4,12 @@ set -e
 ls -l
 #echo "${LDC_TARGET}:${LDC_USERNAME}@${LDC_PASSWORD}"
 
-ldc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
-ldc add-env GoWakaEnv -n APP_NAME -v $APP_NAME
-ldc add-env GoWakaEnv -n APP_CLIENT_ID -v $APP_CLIENT_ID
-ldc add-env GoWakaEnv -n APP_CLIENT_SECRET -v $APP_CLIENT_SECRET
-ldc add-env GoWakaEnv -n GW_DB_USER -v $GW_DB_USER
-ldc add-env GoWakaEnv -n GW_DB_PWD -v $GW_DB_PWD
-ldc get-env GoWakaEnv
-ldc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
+lc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
+lc add-env GoWakaEnv -n APP_NAME -v $APP_NAME
+lc add-env GoWakaEnv -n APP_CLIENT_ID -v $APP_CLIENT_ID
+lc add-env GoWakaEnv -n APP_CLIENT_SECRET -v $APP_CLIENT_SECRET
+lc add-env GoWakaEnv -n GW_DB_USER -v $GW_DB_USER
+lc add-env GoWakaEnv -n GW_DB_PWD -v $GW_DB_PWD
+lc get-env GoWakaEnv
+lc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
 
