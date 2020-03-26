e8fb1a4e911d86d8d5104f32c9f797813df95205
Edward Tanko <eddytnk@gmail.com>
2020-03-26 18:56:40 -0500

    add credentials to pipeline - [#171976240]

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -10,6 +10,8 @@ lc add-env GoWakaEnv -n APP_CLIENT_ID -v $APP_CLIENT_ID
 lc add-env GoWakaEnv -n APP_CLIENT_SECRET -v $APP_CLIENT_SECRET
 lc add-env GoWakaEnv -n GW_DB_USER -v $GW_DB_USER
 lc add-env GoWakaEnv -n GW_DB_PWD -v $GW_DB_PWD
+lc add-env GoWakaEnv -n PAYAMGO_CLIENT_SECRET -v $PAYAMGO_CLIENT_SECRET
+lc add-env GoWakaEnv -n PAYAMGO_CLIENT_KEY -v $PAYAMGO_CLIENT_KEY
 lc get-env GoWakaEnv
 lc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
 
