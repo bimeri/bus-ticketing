8ff10fde4feec415a84563e83668f020afa701d5
Edward Tanko <eddytnk@gmail.com>
2020-04-21 22:14:18 -0500

    Add Setup Gowaka Acceptance environment - [#172447890]

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -2,16 +2,18 @@
 
 set -e
 ls -l
-#echo "${LDC_TARGET}:${LDC_USERNAME}@${LDC_PASSWORD}"
 
 lc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
 lc add-env GoWakaEnv -n APP_NAME -v $APP_NAME
+lc add-env GoWakaEnv -n GW_DB_NAME -v $GW_DB_NAME
+lc add-env GoWakaEnv -n APP_CLIENT_URL -v $APP_CLIENT_URL
 lc add-env GoWakaEnv -n APP_CLIENT_ID -v $APP_CLIENT_ID
 lc add-env GoWakaEnv -n APP_CLIENT_SECRET -v $APP_CLIENT_SECRET
 lc add-env GoWakaEnv -n GW_DB_USER -v $GW_DB_USER
 lc add-env GoWakaEnv -n GW_DB_PWD -v $GW_DB_PWD
 lc add-env GoWakaEnv -n PAYAMGO_CLIENT_SECRET -v $PAYAMGO_CLIENT_SECRET
 lc add-env GoWakaEnv -n PAYAMGO_CLIENT_KEY -v $PAYAMGO_CLIENT_KEY
+lc add-env GoWakaEnv -n AS_PUBLIC_KEY -v $AS_PUBLIC_KEY
 lc get-env GoWakaEnv
 lc push  --app-dir minio-s3/*.jar --config source-code-from-gitlab/ci/appconfig.json
 
