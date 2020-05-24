aab73ba165f2f9fe5422d4c61b67081c1628a407
Edward Tanko <eddytnk@gmail.com>
2020-05-24 17:47:40 -0500

    Add new LC deployment

--- ci/tasks/deploy-dev.sh
+++ ci/tasks/deploy-dev.sh
@@ -4,7 +4,6 @@ set -e
 ls -l
 ls -l source-code-from-gitlab/ci
 #
-#lc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
 #lc add-env GoWakaEnv -n APP_NAME -v $APP_NAME
 #lc add-env GoWakaEnv -n GW_DB_NAME -v $GW_DB_NAME
 #lc add-env GoWakaEnv -n APP_CLIENT_URL -v $APP_CLIENT_URL
@@ -16,6 +15,8 @@ ls -l source-code-from-gitlab/ci
 #lc add-env GoWakaEnv -n PAYAMGO_CLIENT_KEY -v $PAYAMGO_CLIENT_KEY
 #lc add-env GoWakaEnv -n AS_PUBLIC_KEY -v $AS_PUBLIC_KEY
 #lc get-env GoWakaEnv
+
+lc login -t $LDC_TARGET -u $LDC_USERNAME -p $LDC_PASSWORD
 
 lc push --app-file minio-s3/*.jar --config $PWD/source-code-from-gitlab/ci/config.yml --env "APP_NAME=${APP_NAME},GW_DB_NAME=$GW_DB_NAME,APP_CLIENT_URL=${APP_CLIENT_URL},APP_CLIENT_ID=${APP_CLIENT_ID},APP_CLIENT_SECRET=${APP_CLIENT_SECRET},GW_DB_USER=${GW_DB_USER},GW_DB_PWD=${GW_DB_PWD},PAYAMGO_CLIENT_SECRET=${PAYAMGO_CLIENT_SECRET},PAYAMGO_CLIENT_KEY=${PAYAMGO_CLIENT_KEY},AS_PUBLIC_KEY=${AS_PUBLIC_KEY}"
 
