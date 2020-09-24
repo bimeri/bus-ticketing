5d8c62b7fca7104d53c12ed3d230128a208dcd1f
Edward Tanko <eddytnk@gmail.com>
2020-09-23 20:43:47 -0500

    refactor POM.xml file

--- src/main/java/net/gogroups/gowaka/dto/CodeDTO.java
+++ src/main/java/net/gogroups/gowaka/dto/CodeDTO.java
@@ -2,11 +2,13 @@ package net.gogroups.gowaka.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CodeDTO {
     @NotBlank(message = "code is required")
     private String code;
