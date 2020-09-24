5c3aab99d9e9dbd63df0841a186b2e8c66b0c086
Edward Tanko <eddytnk@gmail.com>
2020-09-24 05:03:28 -0500

    fix pom file 3

--- ci/tasks/test-and-build.sh
+++ ci/tasks/test-and-build.sh
@@ -12,7 +12,8 @@ echo "Current directory ..."
 ls
 echo "Packaging jar file"
 ./mvnw clean package
-./mvnw --settings ./settings.xml clean package
+#./mvnw --settings ./settings.xml clean package
+./mvnw --settings ../nexus-settings/settings.xml clean package
 echo "Current directory ..."
 ls
 echo "copying jar to ../jar-file directory"
