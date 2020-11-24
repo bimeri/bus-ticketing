3f84e0d07aa75f395f8edc53c9131ee872c5dc47
Edward Tanko <eddytnk@gmail.com>
2020-11-23 18:48:49 -0600

    Add image deploy script

---
+++ ci/tasks/deploy-containarized-app.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+pwd
+ls -l
+
+appName=$LCS_APPNAME
+appType=$LCS_APPTYPE
+port=$LCS_PORT
+version=$LCS_VERSION
+registry=$LCS_REGISTRY
+username=$LCS_USERNAME
+password=$LCS_PASSWORD
+url=$LCS_URL
+
+echo "${version}"
+
+echo "{
+  \"appName\": \"${appName}\",
+  \"appType\": \"${appType}\",
+  \"password\": \"${password}\",
+  \"port\": ${port},
+  \"registry\": \"${registry}\",
+  \"username\": \"${username}\",
+  \"version\": \"${version}\"
+}" >./config.json
+
+curl -F 'appfile=@minio-s3/*.jar' -F 'config=@./config.json' "${url}"
